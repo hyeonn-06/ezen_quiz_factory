76940b2555cb46bfbb99617c4c3bf8fbf5108fe9
김도현 <153271427+hyeonn-06@users.noreply.github.com>
2025-07-11 14:45:50 +0900

    Add files via upload

---
+++ boot/qf/src/main/java/com/qf/quiz/vo/QuizVO.java
@@ -0,0 +1,15 @@
+package com.qf.quiz.vo;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class QuizVO {
+	private Long quiz_no;
+	private String title;
+	private String content;
+	private String file_path;
+	private String member_id;
+	private Date created_at;
+}
